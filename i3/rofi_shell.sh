8260aa2f12c70ea5c495883068d24ee2de222c1b
Andrew Barlow <dandrewbarlow@gmail.com>
2018-06-13 03:29:29 +0000

    lots of tweaking

--- i3/rofi_shell.sh
+++ i3/rofi_shell.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-bg_color='#38B6FF'
-text_color='#B338FF'
-htext_color='#8138FF'
+bg_color=$1
+text_color=$2
+htext_color=$3
 
-rofi -show run -lines 3 -eh 2 -width 100 -padding 400 -opacity "99" -bw 0 -color-window "$bg_color, $bg_color, $bg_color" -color-normal "$bg_color, $text_color, $bg_color, $bg_color, $htext_color" -font "haxor 14"
+rofi -show run -lines 6 -eh 2 -width 30 -padding 10 -opacity "90" -bw 0 -color-window "$bg_color, $bg_color, $bg_color" -color-normal "$bg_color, $text_color, $bg_color, $bg_color, $htext_color" -font "haxor 14"
