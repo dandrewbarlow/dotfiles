d9bcce1eda556f6a851bb9a55ee30e21efd2e83c
Andrew Barlow <dandrewbarlow@gmail.com>
2018-06-26 15:44:33 -0600

    removed old files

--- i3/rofi_shell.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-bg_color=$1
-text_color=$2
-htext_color=$3
-
-rofi -show run -lines 6 -eh 2 -width 30 -padding 10 -opacity "90" -bw 0 -color-window "$bg_color, $bg_color, $bg_color" -color-normal "$bg_color, $text_color, $bg_color, $bg_color, $htext_color" -font "haxor 14"
--- i3/testScript.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-
-ticker=1
-
-while [ $ticker -lt 100 ]; do
-	./randomWallpaper.sh
-	let ticker=ticker+1
-done
