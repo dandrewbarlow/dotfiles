1f945abd87472f29145796125915e56f00af2b52
Andrew Barlow <andrew@localhost.localdomain>
2020-08-20 15:45:47 -0700

    created script to set git correctly

---
+++ git-config.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+
+echo "Enter global user name for git"
+read username
+git config --global user.name $username
+
+echo "Enter global user email for git"
+read email
+git config --global user.email $email
