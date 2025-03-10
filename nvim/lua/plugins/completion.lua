690d1cc634a2fe5fbf7c3cac8e1a1c58433ab562
Andrew Barlow <dandrewbarlow@gmail.com>
2025-03-10 03:39:12 -0500

    Separate nvim dotfiles into seperate repo
    keeping it modular, yo

--- nvim/lua/plugins/completion.lua
+++
@@ -1,9 +0,0 @@
-return {
-  {
-    -- cmp: neovim completion engine
-    "hrsh7th/nvim-cmp",
-    dependencies = require('plugins.config.cmp').dependencies,
-    config = require('plugins.config.cmp').config,
-
-  }
-}
