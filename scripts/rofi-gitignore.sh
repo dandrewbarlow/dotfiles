d29617ddc75ef938e69b1c2bc64fd99a0d060d45
Andrew Barlow <dandrewbarlow@gmail.com>
2025-06-01 21:56:35 -0500

    delete some old scripts

--- scripts/rofi-gitignore.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/bash
-
-# git pull "$HOME/.scripts/gitignore/" || notify-send ["gitignore script"] "gitignore repo failed to update"
-
-file="$(ls "$HOME/.scripts/gitignore/" | rofi -dmenu -i -p "Choose gitignore to copy to pwd" | awk '{printf"/home/andrew/.scripts/gitignore/%s", $1}')"
-
-cp "$file" "$(pwd)"
