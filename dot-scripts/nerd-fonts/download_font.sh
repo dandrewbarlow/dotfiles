56d63441ca9bbebcfcf2a5b4ea2cf0fe9de2fa46
Andrew Barlow <dandrewbarlow@gmail.com>
2026-09-01 20:47:12 -0500

    remove old nerd fonts dir to move to my newer version

--- dot-scripts/nerd-fonts/download_font.sh
+++
@@ -1,7 +0,0 @@
-#! /usr/bin/env bash
-
-tmp="$(mktemp -d)"
-
-cd "$(tmp)" && curl -OL https://github.com/ryanoasis/nerd-fonts/releases/latest/download/JetBrainsMono.tar.xz 
-
-tar -xf ./JetBrainsMono.tar.xz
